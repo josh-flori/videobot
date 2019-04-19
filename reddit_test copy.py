e7d76852a924be643ffa8344eb48a5877efed85d
josh flori <josh.flori@gatewayblend.com>
2019-04-18 23:26:47 -0500

    some more image additions....
    this is practically half way done (tedious stuff to do now though)

--- reddit_test copy.py
+++ reddit_test copy.py
@@ -5,11 +5,16 @@
 #####################
 from PIL import Image, ImageDraw, ImageFont
 # create image
-img = Image.new('RGB', (1920,1080), color = (73, 109, 137))
+img = Image.new('RGB', (1920,1080), color = (26, 26, 26))
 # set font
-fnt = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 15)
+fnt = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 40)
 # draw image
 d = ImageDraw.Draw(img)
 # first argument is xy coordinate of the top left of the text. DOPE!
-d.text((800,1000), "I work at an Italian place right now ", font=fnt, fill=(255, 255, 0))
+d.text((800,1000), "I work at an Italian place right now ", font=fnt, fill=(240, 240, 240))
+# add two texts....
+d.text((800,100), "I work at an Italian place right now ", font=fnt, fill=(240, 240, 240))
 img.save('pil_text_font.png')
+
+
+# for creating movie... https://github.com/Zulko/moviepy
