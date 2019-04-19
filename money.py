cae6ab1fd69675ef216ae81c9bd22a4596275a9c
josh flori <josh.flori@gatewayblend.com>
2019-04-19 08:45:54 -0500

    desription changes

---
+++ money.py
@@ -0,0 +1,4 @@
+# looks like CB makes 3,000 per 2million views, or .0015 per view
+# this ask reddit channel has 800k in a week 
+# https://www.youtube.com/channel/UCK0vzszR4DdSsD1KM5jdfcA/videos
+# which is 3.2million a month, which at CB's monetization rate is $4,800 a month, or $57,600 a year. And the views on this channel are small compared to the same guy's other reddit channel, which is getting, i didn't count, but probably 10mill+ a month. The revenue potential here is conversatively 50k, up to 100k a year, I think, IF the channel can get traction, which I think it should. It's a hot topic right now. And no one but this guy seems to be doing content at scale. 
