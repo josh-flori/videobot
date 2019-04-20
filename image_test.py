f4ef5d7248c86c9aa8053118aa47c36dbc9590c0
josh flori <josh.flori@gatewayblend.com>
2019-04-19 19:55:13 -0500

    updates

--- image_test.py
+++ image_test.py
@@ -12,32 +12,75 @@ import numpy as np
 import itertools
 
 comment= """That she can't have a relationship with her grandfather because he's a pedophile and I would never trust him. The rest of my family maintains a relationship with him and leans on me hard to open up communication because "family comes first." They are absolutely right, my family does come first, which is why my daughter won't ever have to have a relationship with him."""
-
-
-def split_seq(iterable, size):
-    it = iter(iterable)
-    item = list(itertools.islice(it, size))
-    while item:
-        yield item
-        item = list(itertools.islice(it, size))
-
-
-
-######################
-# SET SOME THINGS UP #
-######################
-chunked = list(split_seq(TweetTokenizer().tokenize(comment),7))
-# create image
-img = Image.new('RGB', (1920,1080), color = (26, 26, 26))
-# set font
-fnt1 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 30)
-fnt2 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 40)
 # user
 user="bla"
 # age
 age = "3 months ago"
 # updoots
 updoots = updoots
+
+######################
+# SET SOME THINGS UP #
+######################
+count = 0
+# holds total chunked list 
+chunked=[]
+# holds each letter as the for loop progress, dumped into chunked once character limit or sentence end is reached
+temp=[]
+# contains information about whether that chunk is the end of a sentence, to be passed in when creating the images so that the image thing knows to put the next chunk on the same line rather than the next line
+is_eos=[]
+# when the above is triggered, the image thing needs to know how far to indent the next chunk on the same line, so this will govern that
+chunk_len=[]
+# loop through each letter
+for i in range(len(comment)):
+    # append that letter to temporary holding list
+    temp.append(comment[i])
+    # increase count of letters
+    count+=1
+    # test to see if the sentence has ended before the end of the line
+    if comment[i] == "." or comment[i] == "!" or comment[i] == "?" and comment[i+1]==" ":
+        # join letters together into single string
+        dump="".join(temp)
+        # append to chunked
+        chunked.append(dump)
+        # reset temporary holding list
+        temp=[]
+        # this will be used to govern the x_adjust parameter in the image thing
+        is_eos.append(True)
+        # this will be used to govern the x_adjust parameter in the image thing
+        chunk_len.append(len(dump))
+    # test if end of line has been reached
+    if count >67:
+        if comment[i]==" ":
+            dump="".join(temp)
+            chunked.append(dump)
+            temp=[]
+            count=0
+            is_eos.append(False)
+            chunk_len.append(0) 
+    # true when reached the end of the comment and no other condition was met
+    elif i == len(comment)-1:
+        dump="".join(temp)
+        chunked.append(dump)
+        
+# get them together for easier processing
+parameters=list(zip(chunked,is_eos,chunk_len))
+
+
+
+
+
+
+
+
+
+
+
+# create image
+img = Image.new('RGB', (1920,1080), color = (26, 26, 26))
+# set font
+fnt1 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 30)
+fnt2 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 40)
 
 
 ####################
@@ -52,32 +95,16 @@ d.text((100+len(user)*27,210), "21k points", font=fnt1, fill=(170, 170, 170))
 d.text((100+len(user)*27+174,210), '\u00B7 2 days ago', font=fnt1, fill=(170, 170, 170))
 
 
-# measures how many characters have accumulated on that line
-fill=0
-# determines if write to same line
-same_line=True
+
+
 # control movement parameters
-x_adjust=0
 vertical=0
 
 for i in range(len(chunked)):
-    # this starts at 0, will increment with each new line, controls the vertical dimension of where the line is typed, adjusts as "same_line" variable adjusts
-    chunk_length=len("".join(chunked[i]))
-    fill+=chunk_length
-    print(fill)
-    if fill >= 67:
-        same_line=False
-        # reset to chunk length
-        fill=chunk_length
-        # bump to next line
-        vertical+=1
-        x_adjust=0
-    else:
-        same_line=True
     # add comments, using different text (bigger)
-    d.text(((25*x_adjust)+200,(vertical*60)+260), " ".join(chunked[i]), font=fnt2, fill=(240, 240, 240))
+    d.text(((25*x_adjust)+200,(vertical*60)+260), chunked[i], font=fnt2, fill=(240, 240, 240))
     img.save('/users/josh.flori/desktop/pil_text_font'+str(i)+'.png')
-    x_adjust+=chunk_length    
+    vertical+=1    
 
 
 
