0719ee6e1615e23cfdccb3f5bef81e8fb18342eb
josh flori <josh.flori@gatewayblend.com>
2019-04-19 21:23:00 -0500

    ...
    video functional.. approximately right timing... need to sync up audio now

--- image_test.py
+++ image_test.py
@@ -1,6 +1,6 @@
 # /users/josh.flori/desktop/test/bin/python3 /users/josh.flori/documents/josh-flori/video-creator/reddit_test.py
 
-# estimated average characters per minute: 652.5
+# estimated average characters per second: 10.875
 # max characters per line = 67
 # first argument is xy coordinate of the top left of the text. DOPE!
 
@@ -31,6 +31,8 @@ temp=[]
 # contains information about whether that chunk is the end of a sentence, to be passed in when creating the images so that the image thing knows to put the next chunk on the same line rather than the next line
 is_eos=[]
 # when the above is triggered, the image thing needs to know how far to indent the next chunk on the same line, so this will govern that
+indent=[]
+# used for setting movie duration for that frame
 chunk_len=[]
 # loop through each letter
 for i in range(len(comment)):
@@ -46,13 +48,15 @@ for i in range(len(comment)):
             temp=[]
             count=0
             is_eos.append(False)
-            chunk_len.append(0) 
+            indent.append(0) 
+            chunk_len.append(len(dump))
     # true when reached the end of the comment and no other condition was met
     elif i == len(comment)-1:
         dump="".join(temp)
         chunked.append(dump)
         is_eos.append(False)
-        chunk_len.append(0)
+        indent.append(0)
+        chunk_len.append(len(dump))
     # test to see if the sentence has ended before the end of the line
     elif all([any([comment[i] == "." or comment[i] == "!" or comment[i] == "?"]),comment[i+1]==" "]):
         # join letters together into single string
@@ -64,6 +68,7 @@ for i in range(len(comment)):
         # this will be used to govern the x_adjust parameter in the image thing
         is_eos.append(True)
         # this will be used to govern the x_adjust parameter in the image thing
+        indent.append(len(dump))
         chunk_len.append(len(dump))
     # another test, based on a sentence ending in a quote...
     elif all([any([comment[i-1]==".",comment[i-1]=="?",comment[i-1]=="!"]),comment[i]=='"' and comment[i+1]==" "]):
@@ -71,11 +76,12 @@ for i in range(len(comment)):
         chunked.append(dump)
         temp=[]
         is_eos.append(True)
-        chunk_len.append(len(dump))        
+        indent.append(len(dump))     
+        chunk_len.append(len(dump))
         
-assert(len(chunked)==len(is_eos)==len(chunk_len))
+assert(len(chunked)==len(is_eos)==len(indent)==len(chunk_len))
 # get them together for easier processing
-parameters=list(zip(chunked,is_eos,chunk_len))
+parameters=list(zip(chunked,is_eos,indent,chunk_len))
         
 # we now need to determine how many lines there are. We can't fit more than x lines on an image. Two things must occur. we must check if lines exceed limit, if so, remove comment and don't include it in the video. also return number of lines, which will be used to adjust the y_adjust in the image
 num_lines=int(len(is_eos)-is_eos.count(True))
@@ -112,7 +118,8 @@ d.text((100+len(user)*27+174,510-(num_lines*30)), '\u00B7 2 days ago', font=fnt1
 #################
 line=0
 x_adjust=0
-# parameters[i][0]==chunk, parameters[i][1]==is_eos, parameters[i][0]==chunk_len 
+image_paths=[]
+# parameters[i][0]==chunk, parameters[i][1]==is_eos, parameters[i][2]==indent, parameters[i][0]==chunk_len
 for i in range(len(parameters)):
     # so the point of x_adjust is when a sentence ends mid-line, we want to indent the next chunk on the same line. this is true when the previous chunk was a sentence that ended mid-line
     if i!=0:
@@ -122,6 +129,7 @@ for i in range(len(parameters)):
             x_adjust=0
     d.text(((20.4*x_adjust)+100,560-(num_lines*30)+(line*60)), parameters[i][0], font=fnt2, fill=(240, 240, 240))
     img.save('/users/josh.flori/desktop/pil_text_font'+str(i)+'.png')
+    image_paths.append('/users/josh.flori/desktop/pil_text_font'+str(i)+'.png')
     if parameters[i][1]!=True:
         line+=1    
 
@@ -132,8 +140,15 @@ for i in range(len(parameters)):
 
 # for creating movie... https://github.com/Zulko/moviepy
 
+# seconds=
+from moviepy.editor import *
 
+imgs = ['/users/josh.flori/desktop/pil_text_font0.png', '/users/josh.flori/desktop/pil_text_font1.png']
 
+clips = [ImageClip(imgs[i]).set_duration(parameters[i][3]/10.352) for i in range(len(img))]
+
+concat_clip = concatenate_videoclips(clips, method="compose")
+concat_clip.write_videofile("test.mp4", fps=24)
 
 
 
