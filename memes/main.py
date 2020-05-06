291f815b2739c3c9bb0cf595fe1a042d4f30eb0c
josh-flori <joshuaflori@gmail.com>
2020-05-05 20:33:07 -0500

    cleaned up main.py... at this point we are good to go with using it!

--- memes/main.py
+++ memes/main.py
@@ -1,4 +1,4 @@
-from memes import get_image_data_from_reddit, text, frames, processing, config, process_audio, make_video
+from memes import reddit, text, frames, processing, config, audio, make_video
 import os
 import cv2
 from google.cloud import automl
@@ -29,60 +29,38 @@ limit = 1
 all_audio_text = []  # chunked text at paragraph level to create audio files
 
 for i in range(limit):
-    ############
-    # PART 1: GET IMAGE API DATA (TEXT/CUSTOM MODEL BOUNDING BOX DATA
-    ############
-
-    # LOAD IMAGE
+    # PART 1: GET MEME DATA FROM APIs (VISION & AUTO_ML)
     image = cv2.imread('/users/josh.flori/desktop/memes/' + str(i) + '.jpg')
-    # GET IMAGE TEXT FROM GOOGLE API
     raw_text_response = text.get_image_text_from_google('/users/josh.flori/desktop/memes/' + str(i) + '.jpg')
-    # CREATE TEXT BOUNDING BOXES WITH A BUNCH OF RULES AND EXCLUSIONS BUILT IN
     text_boxes, raw_text = text.create_blocks_from_paragraph(raw_text_response)
-    # GET MODEL BOUNDING BOXES
     annotation = frames.get_frame_prediction_from_google(meme_path, i, config.custom_model_project_id,
                                                          config.custom_model_model_id)
 
-    ############
     # PART 2: CLEAN UP THE DATA
-    ############
-
-    # CLEAN UP ANNOTATION
     frames.expand_to_edge(annotation)
-    # CREATE BLOCKS FROM ANNOTATION
     frame_boxes = frames.create_blocks_from_annotations(annotation, image)
-    # CLEAN UP ANNOTATION AGAIN
     frames.trim_white_space(image, frame_boxes)
 
-    ############
     # PART 3: CREATE MASTER LIST OF BOX OBJECTS
-    ############
-    # COMBINE TEXT/CUSTOM MODEL BOXES
     all_boxes = text_boxes + frame_boxes
-    # SORT BY Y, THEN X
-    all_boxes = sorted(all_boxes, key=lambda x: (x[0][1], x[0][0]))
-    # MORE CLEANING UP - FOR ANY BOXES THAT SHOULD BE ALIGNED HORIZONTALLY, ALIGN THEM
+    all_boxes = sorted(all_boxes, key=lambda x: (x[0][1], x[0][0]))  # SORT BY Y, THEN X
     processing.align_tops(all_boxes)
-    # RESORT NOW THAT THINGS ARE ALIGNED...
-    all_boxes = sorted(all_boxes, key=lambda x: (x[0][1], x[0][0]))
+    all_boxes = sorted(all_boxes, key=lambda x: (x[0][1], x[0][0]))  # RESORT NOW THAT THINGS ARE ALIGNED...
     true_sorted_boxes = processing.true_sort(all_boxes)
     slide_text = processing.write_images(image, true_sorted_boxes, meme_output_path, i)
     slide_text = processing.clean_slide_text(slide_text)
-    ############
+
     # PART 4: CREATE AUDIO CLIPS
-    ############
-    # CONVERT ALL_BOXES INTO TEXTUAL REPRESENTATION OF WHAT IS HAPPENING
     box_text_type = processing.encode_box_text_type(true_sorted_boxes)
-    # CONVERT THAT INTO TEXT THAT CAN BE FED TO AUDIO FUNCTION
     audio_text = processing.get_audio_text(box_text_type, raw_text)
     all_audio_text.append(audio_text)
-    process_audio.create_mp3s(audio_text, i, audio_output_path, padding_dir)
+    audio.create_mp3s(audio_text, i, audio_output_path, padding_dir)
 
+    # PART 5: CREATE VIDEO
     slide_durations = make_video.compute_slide_duration(audio_output_path, audio_text, slide_text, i,
                                                         '/users/josh.flori/desktop/padding.mp3')
     make_video.create_video(slide_durations, meme_output_path, audio_output_path, 'out.mp3', i)
 
+
 response = model_client.undeploy_model(model_full_id)
 print("Model un-deployment finished. {}".format(response.result()))
-
-
